5f8a1ebd25e0a0b7b3954f44074b05d64197060f
Saniya0710 <saniyatabassum6303@gmail.com>
2023-01-19 00:17:01 +0530

    added settimeout t

--- test.js
+++ test.js
@@ -4,3 +4,15 @@ console.log("test is called")
 }
 
 test()
+//ASynchronous
+
+console.log("customer-1 ordered biryani")
+console.log("customer-2 ordered water bottle")
+
+setTimeout(()=>{
+    console.log("biryani delivered to customer-1")
+}, 5000)
+
+setTimeout(()=>{
+    console.log("Waterbottle delivered to customer-2")
+}, 1000)
